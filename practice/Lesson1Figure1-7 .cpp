f44f124a8809f5e15d6aa327887bccd96c4b60d1
Elita Danilyuk <elitadanilyuk@gmail.com>
2020-05-31 15:38:16 -0700

    adding lesson 1 figure 1-7 example

---
+++ practice/Lesson1Figure1-7 .cpp
@@ -0,0 +1,30 @@
+// Elita Danilyuk
+// 31 May 2020
+
+#include <iostream>
+
+using namespace std;
+
+// start
+int main()
+{
+   // declare variables - learn more in lesson 2
+   int myNumber;  // a variable to collect the user input
+   int myAnswer;  // a variable to store the calculation result
+
+   // input number
+   cin >> myNumber;
+   // myAnswer = myNumber * 2
+   myAnswer = myNumber * 2; // 2 is an unnamed numeric constant, which we do not
+                            // want in our programs. Learn how in lesson 2.
+   // output myAnswer
+   cout << myAnswer << endl;
+
+   return 0;   
+}  // end main() Always label the closing curly - required in this class.
+// stop
+
+// When you execute this program, it may be a bit disconcerting that all you see is 
+// a blinking cursor.  
+// This is an example of the worst user interface.  Well placed prompts will
+// be the remedy.  Learn more in lesson 2.  
