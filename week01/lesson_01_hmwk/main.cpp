b8e78815204ec8c470097f07417f936d500c43c5
Elita Danilyuk <71053850+elitadanilyuk@users.noreply.github.com>
2022-04-04 13:24:36 -0600

    Create main.cpp

---
+++ week01/lesson_01_hmwk/main.cpp
@@ -0,0 +1,26 @@
+//
+//  Lesson 1 Homework - Elita Danilyuk
+//
+//  Created by Elita on 5/31/20.
+//
+
+#include <iostream>
+using namespace std;
+
+// start
+int main ()
+{
+    // output name
+    cout << "Elita Danilyuk"<< endl;
+    
+    // output address, my favorite fake address Dunder Mifflin Paper Company
+    cout << "1725 Slough Avenue, Scranton, PA 18505"<< endl;
+    
+    // output telphone number
+    cout << "540-383-6521"<< endl;
+    
+    // output intended course major
+    cout << "Computer Science"<< endl;
+    return 0;
+} // end main()
+// stop
